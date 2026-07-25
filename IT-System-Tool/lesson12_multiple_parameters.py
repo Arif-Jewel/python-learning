268551833b1e14aebbd20bc948af93c227903ee9
Arif Jewel <jewel24719@yahoo.com>
2026-07-25 23:54:41 +0100

    Multiple parameters

---
+++ IT-System-Tool/lesson12_multiple_parameters.py
@@ -0,0 +1,12 @@
+print("=" * 40)
+print("     MULTIPLE PARAMETERS")
+print("=" * 40)
+
+def add_numbers(first_number, second_number):
+    return first_number + second_number
+
+result = add_numbers(15, 25)
+
+print("Result: ", result)
+
+print("Program finished.")
